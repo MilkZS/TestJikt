56c167860ea18681c52ceb9095b31518030785d5
MilkZS <774765813@qq.com>
2019-12-07 11:54:01 +0800

    test3:

---
+++ app/src/main/java/com/milkzz/test/Test.java
@@ -0,0 +1,14 @@
+package com.milkzz.test;
+
+import android.util.Log;
+
+/**
+ * Created by zuoqi on 2019/12/7
+ */
+public class Test {
+
+    public void show(){
+        Log.d("ZZZ","i amn here");
+    }
+
+}
